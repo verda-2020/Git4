ea6daf0a0e99040fcbafdf3f2aaa003be69d5182
nuraimmoldosenova <msufuk@gmail.com>
2022-09-18 12:38:31 -0400

    We create Car class for first commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         Car car = new Car("Honda Civic", "white");
+        Car car = new Car("Honda Civic Plus", "black");
     }
 }
